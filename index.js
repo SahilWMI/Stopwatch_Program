e9976c60fd0719f468e64e0a336aa6705bea8347
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-06-15 23:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function start(){
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+    }
 }
 function stop(){
 
 }
 function reset(){
+
+}
+function update{
 
 }
